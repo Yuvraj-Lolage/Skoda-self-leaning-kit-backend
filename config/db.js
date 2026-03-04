805e115ef26ee0042f4885f0202d7513af53ff25
yuvraj satish lolage <yuvraj.lolage7@gmail.com>
2026-03-04 09:40:04 +0530

    added hosted MYSQL configuration and removed env and node modules

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: 'localhost',       
-  user: 'root',            
-  password: 'yuvraj@123',
-  database: 'skoda_self_learning_kit',
+  host: process.env.MYSQL_HOST,       
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,            
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
